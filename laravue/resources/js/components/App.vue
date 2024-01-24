7ea4a9d178ef45eac1e7dde6d6709e2772b506d0
delfini <delfini.lucas@hotmail.com>
2024-01-23 23:27:48 -0300

    first commit

---
+++ laravue/resources/js/components/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2> App vindo da aplicação vue</h2>
+</template>
---
+++ laravue/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('content')
+        <h2> teste vue js </h2>
+
+        <App />
+@endsection
